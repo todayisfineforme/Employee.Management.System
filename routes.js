1ccf35d02f8a9e681219813a87309ff00d3759f7
Nathan Hall <Nashing1@gmail.com>
2020-08-16 15:06:04 -0500

    added starting template
    added in files for a starter mysql app. connection is live and app is stable. hitting the status route receives a good response.

---
+++ routes.js
@@ -0,0 +1,8 @@
+exports.route = (app, connection) => {
+    app.get('/status', (request,response) => {
+        const status = {
+            status: 'ok'
+        }
+        response.json(status);
+    });
+}
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
+const connection = require('./connection');
+
+const app = express();
+const PORT = 9000;
+
+const run = async (app) => {
+    const connected = await connection.connect();
+    routes.route(app, connected);
+}
+
+
+app.use(cors());
+app.use(express.urlencoded({extended: true}));
+app.use(bodyParser.json());
+run(app);
+
+app.listen(PORT, () => console.log(`API is listening on port ${PORT}`));
